c0ffef78cb146bd65458d0b6421725dcb0a9eba8
Renan Guedes <renanguedes.br@gmail.com>
2020-02-14 09:50:59 -0300

    my card finished

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import { useSpring, animated } from 'react-spring'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useSpring, animated } from 'react-spring';
+import './styles.css'
 
-function Demo() {
-    const [state, toggle] = useState(true)
-    const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0, config: { duration: 1000 } })
-    return (
-      <div onClick={() => toggle(!state)}>
-        <animated.div
-          style={{
-            opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
-            transform: x
-              .interpolate({
-                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
-              })
-              .interpolate(x => `scale(${x})`)
-          }}>
-          click
-        </animated.div>
-      </div>
-    )
-  }
+const calc = (x,y) => [-(y - window.innerHeight/2)/20, (x - window.innerWidth/2)/20, 1.1]
+const trans = (x,y,s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+export default function Card () {
+  const [ props, set ] = useSpring(() => ({ xys: [0,0,1], 
+    config: {
+      mass: 10,
+      tension: 350,
+      friction: 40,
+    }
+  }))
   
-  ReactDOM.render(<Demo />, document.getElementById('root'))
+  return (
+    <div className='container'>
+      <animated.div 
+        className='card'
+        onMouseMove={({ clientX:x, clientY:y }) => set({ xys: calc(x,y) })}
+        onMouseLeave={() => set({ xys: [0,0,1]})}
+        style={{
+          transform: props.xys.interpolate(trans)
+        }}
+      />
+    </div>
+  )
+}
+
+ReactDOM.render(<Card />,document.getElementById('root'))
